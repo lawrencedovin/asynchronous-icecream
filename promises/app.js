9cd85fa2ace858fb187f83d836162445fa930ad7
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-23 08:42:27 -0500

    Updated promises file to have catch and finally

--- promises/app.js
+++ promises/app.js
@@ -15,7 +15,7 @@ let order = (work, time) => {
             }, time)
         }
         else {
-            reject(reject(console.log("our shop is closed")));
+            reject(reject(console.log("Our shop is closed.")));
         }
     });
  };
@@ -23,12 +23,14 @@ let order = (work, time) => {
 
 // The work parameter is a callback that executes in the order function.
 // Passes an anonymous function as a work parameter.
+// .catch() only works when Promise is rejected.
+// .finally() works when Promise is Resolved or Rejected.
 order(() => console.log(`${stocks.fruits[1]} was cut.`), 2000)
 .then(() => {
     return order(() => console.log(`Added ${stocks.liquid[0]} and ${stocks.liquid[1]}.`), 2000)
 })
 .then(() => {
-    return order(() => console.log(`Start the machine.`), 1000)
+    return order(() => console.log(`Started the machine.`), 1000)
 })
 .then(() => {
     return order(() => console.log(`Selected ${stocks.holder[0]}`), 2000)
@@ -37,6 +39,11 @@ order(() => console.log(`${stocks.fruits[1]} was cut.`), 2000)
     return order(() => console.log(`Selected ${stocks.toppings[0]}`), 3000)
 })
 .then(() => {
-    return order(() => console.log(`Serve Ice Cream`), 2000)
+    return order(() => console.log(`Ice Cream is Served!`), 2000)
 })
-
+.catch(() => {
+    console.log("Customer left.");
+})
+.finally(() => {
+    console.log("Day ended, shop is closed.");
+})
